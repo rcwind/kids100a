bcf9917f5327e34dcbd902c185c2cf5f500d1328
rcwind <251349552@qq.com>
2020-12-30 13:03:07 +0800

    modified optical data type

--- opflow/src/pose_data.py
+++ opflow/src/pose_data.py
@@ -2,39 +2,41 @@
 import rospy
 import tf
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Pose2D
+from std_msgs.msg import Float32MultiArray
 
 class optical_flow_conversion:
     def __init__(self):
-        self.optical_flow_pub = rospy.Publisher('/optical/odom', Odometry, queue_size=10)
-        self.optical_flow_sub = rospy.Subscriber('/optical/data', Pose2D, self.callback)
-        self.rate = rospy.Rate(30) # 30Hz because 30fps
+        self.optical_flow_pub = rospy.Publisher('/optical/pose', Odometry, queue_size=10)
+        self.optical_flow_sub = rospy.Subscriber('/optical/data', Float32MultiArray, self.callback)
+        self.rate = rospy.Rate(100) # 100Hz 
         self.optical_odom = Odometry()
 
     def callback(self,data):
-        self.quaternion = tf.transformations.quaternion_from_euler(0,0,data.theta)
-
         self.current_time = rospy.get_rostime()
-        self.optical_odom.pose.pose.position.x = data.x
-        self.optical_odom.pose.pose.position.y = 0.0
-        self.optical_odom.pose.pose.position.z = 0.0
-        self.optical_odom.pose.pose.orientation.x = self.quaternion[0]
-        self.optical_odom.pose.pose.orientation.y = self.quaternion[1]
-        self.optical_odom.pose.pose.orientation.z = self.quaternion[2]
-        self.optical_odom.pose.pose.orientation.w = self.quaternion[3]
-        self.optical_odom.header.frame_id = 'optical'
+
+        self.optical_odom.header.frame_id = 'optical_link'
         self.optical_odom.header.stamp = self.current_time
-        self.optical_odom.pose.covariance[0] = 0.001
-        self.optical_odom.pose.covariance[7] = 0.001
-        self.optical_odom.pose.covariance[14] = 1000000.0
-        self.optical_odom.pose.covariance[21] = 1000000.0
-        self.optical_odom.pose.covariance[28] = 1000000.0
+
+        self.optical_odom.pose.pose.position.x = data.data[2]
+        self.optical_odom.pose.pose.position.y = data.data[3]
+        self.optical_odom.pose.pose.position.z = 0.0
+        self.optical_odom.pose.pose.orientation.x = 0.0
+        self.optical_odom.pose.pose.orientation.y = 0.0
+        self.optical_odom.pose.pose.orientation.z = 0.0
+        self.optical_odom.pose.pose.orientation.w = 0.0
+
+        self.optical_odom.pose.covariance[0] = 0.000001
+        self.optical_odom.pose.covariance[7] = 0.000001
+        self.optical_odom.pose.covariance[14] = 10000000.0
+        self.optical_odom.pose.covariance[21] = 10000000.0
+        self.optical_odom.pose.covariance[28] = 10000000.0
         self.optical_odom.pose.covariance[35] = 0.03
+
         self.optical_flow_pub.publish(self.optical_odom)
         self.rate.sleep()
 
 def main():
-    rospy.init_node('data_converter', anonymous=True)
+    rospy.init_node('data_converter_pose', anonymous=True)
     listener = tf.TransformListener()
 
     ofc = optical_flow_conversion()
--- opflow/src/twist_data.py
+++ opflow/src/twist_data.py
@@ -2,12 +2,11 @@
 import rospy
 import tf
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
 from std_msgs.msg import Float32MultiArray
 
 class optical_flow_conversion:
     def __init__(self):
-        self.optical_flow_pub = rospy.Publisher('/optical/odom', Odometry, queue_size=10)
+        self.optical_flow_pub = rospy.Publisher('/optical/twist', Odometry, queue_size=10)
         self.optical_flow_sub = rospy.Subscriber('/optical/data', Float32MultiArray, self.callback)
         self.rate = rospy.Rate(100) # 100Hz 
         self.optical_odom = Odometry()
@@ -16,23 +15,7 @@ class optical_flow_conversion:
         self.current_time = rospy.get_rostime()
 
         self.optical_odom.header.frame_id = 'optical_link'
-        #  self.optical_odom.child_frame_id = 'base_link'
         self.optical_odom.header.stamp = self.current_time
-
-        self.optical_odom.pose.pose.position.x = data.data[2]
-        self.optical_odom.pose.pose.position.y = data.data[3]
-        self.optical_odom.pose.pose.position.z = 0.0
-        self.optical_odom.pose.pose.orientation.x = 0.0
-        self.optical_odom.pose.pose.orientation.y = 0.0
-        self.optical_odom.pose.pose.orientation.z = 0.0
-        self.optical_odom.pose.pose.orientation.w = 0.0
-
-        self.optical_odom.pose.covariance[0] = 0.001
-        self.optical_odom.pose.covariance[7] = 0.001
-        self.optical_odom.pose.covariance[14] = 1000000.0
-        self.optical_odom.pose.covariance[21] = 1000000.0
-        self.optical_odom.pose.covariance[28] = 1000000.0
-        self.optical_odom.pose.covariance[35] = 0.03
 
         self.optical_odom.twist.twist.linear.x = data.data[0]
         self.optical_odom.twist.twist.linear.y = data.data[1]
@@ -43,23 +26,16 @@ class optical_flow_conversion:
 
         self.optical_odom.twist.covariance[0] = 0.001
         self.optical_odom.twist.covariance[7] = 0.001
-        self.optical_odom.twist.covariance[14] = 0.001
+        self.optical_odom.twist.covariance[14] = 1000000.0
         self.optical_odom.twist.covariance[21] = 1000000.0
         self.optical_odom.twist.covariance[28] = 1000000.0
         self.optical_odom.twist.covariance[35] = 0.03
-
-        # self.optical_odom.twist.covariance[0] = 0.001
-        # self.optical_odom.twist.covariance[7] = 0.001
-        # self.optical_odom.twist.covariance[14] = 0.001
-        # self.optical_odom.twist.covariance[21] = 1000000.0
-        # self.optical_odom.twist.covariance[28] = 1000000.0
-        # self.optical_odom.twist.covariance[35] = 0.03
 
         self.optical_flow_pub.publish(self.optical_odom)
         self.rate.sleep()
 
 def main():
-    rospy.init_node('data_converter', anonymous=True)
+    rospy.init_node('data_converter_twist', anonymous=True)
     listener = tf.TransformListener()
 
     ofc = optical_flow_conversion()
